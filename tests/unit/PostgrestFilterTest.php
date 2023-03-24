9b54f30a2f1a116f599961eac0db1bdbbf021ab3
WichinRighetti <lamrighetti@gmail.com>
2023-03-24 16:36:42 -0700

    Update PostgrestFilterTest.php

--- tests/unit/PostgrestFilterTest.php
+++ tests/unit/PostgrestFilterTest.php
@@ -2,9 +2,13 @@
 
 declare(strict_types=1);
 require 'vendor/autoload.php';
+
+use PhpParser\Node\Expr\Cast\Object_;
+use PhpParser\Node\Name;
 use PHPUnit\Framework\TestCase;
 
 use function PHPUnit\Framework\assertCount;
+use function PHPUnit\Framework\assertEquals;
 
 require __DIR__.'/../../src/PostgrestFilter.php';
 
@@ -26,8 +30,119 @@ final class PostgrestFilterTest extends TestCase
     public function testEq(): void
     {
         $result = $this->filter->eq('name', 'Algeria');
-        assertCount(13, $result);
-        var_dump($result);
+        assertEquals($this->filter->path, $result->{'path'});
+        assertEquals($this->filter->domain, $result->{'domain'});
+        assertEquals($this->filter->scheme, $result->{'scheme'});
+        //assertEquals('eq.Algeria', $result->{'url'}->{'query'});
         ob_flush();
     }
+
+    public function testNeq(): void
+    {
+        $result = $this->filter->neq('name', 'Algeria');
+        assertEquals($this->filter->path, $result->{'path'});
+        assertEquals($this->filter->domain, $result->{'domain'});
+        assertEquals($this->filter->scheme, $result->{'scheme'});
+        //assertEquals('eq.Algeria', $result->{'url'}->{'query'});
+        ob_flush();
+    }
+
+    public function testGt(): void
+    {
+        $result = $this->filter->gt('id', '1');
+        assertEquals($this->filter->path, $result->{'path'});
+        assertEquals($this->filter->domain, $result->{'domain'});
+        assertEquals($this->filter->scheme, $result->{'scheme'});
+        print_r($result);
+        ob_flush();
+    }
+
+    public function testGte(): void
+    {
+        $result = $this->filter->gte('id', '1');
+        assertEquals($this->filter->path, $result->{'path'});
+        assertEquals($this->filter->domain, $result->{'domain'});
+        assertEquals($this->filter->scheme, $result->{'scheme'});
+    }
+
+    public function testLt(): void
+    {
+        $result = $this->filter->lt('id', '1');
+        assertEquals($this->filter->path, $result->{'path'});
+        assertEquals($this->filter->domain, $result->{'domain'});
+        assertEquals($this->filter->scheme, $result->{'scheme'});
+        print_r($result);
+        ob_flush();
+    }
+
+    public function testLte(): void
+    {
+        $result = $this->filter->lte('id', '1');
+        assertEquals($this->filter->path, $result->{'path'});
+        assertEquals($this->filter->domain, $result->{'domain'});
+        assertEquals($this->filter->scheme, $result->{'scheme'});
+    }
+
+    public function testLike(): void
+    {
+        $result = $this->filter->like('name', 'Algeria');
+        assertEquals($this->filter->path, $result->{'path'});
+        assertEquals($this->filter->domain, $result->{'domain'});
+        assertEquals($this->filter->scheme, $result->{'scheme'});
+        print_r($result);
+        ob_flush();
+    }
+
+    public function testiLike(): void
+    {
+        $result = $this->filter->ilike('name', 'Algeria');
+        assertEquals($this->filter->path, $result->{'path'});
+        assertEquals($this->filter->domain, $result->{'domain'});
+        assertEquals($this->filter->scheme, $result->{'scheme'});
+    }  
+    
+    public function testIs(): void
+    {
+        $result = $this->filter->is('name', 'algeria');
+        assertEquals($this->filter->path, $result->{'path'});
+        assertEquals($this->filter->domain, $result->{'domain'});
+        assertEquals($this->filter->scheme, $result->{'scheme'});
+    }   
+
+    public function testIn(): void
+    {
+        $result = $this->filter->in('Algeria', ['countries','id']);
+        print_r($result);
+        ob_flush();
+        assertEquals($this->filter->path, $result->{'path'});
+        assertEquals($this->filter->domain, $result->{'domain'});
+        assertEquals($this->filter->scheme, $result->{'scheme'});
+    }   
+
+    public function testContains(): void
+    {
+        $result = $this->filter->contains('name', 'Algeria');
+        $reflection = new ReflectionClass($result->{'url'}->{'query'});
+        $property = $reflection->getProperty('name');
+        $property->setAccessible(true);
+        print_r($property);
+        ob_flush();
+        assertEquals($this->filter->path, $result->{'path'});
+        assertEquals($this->filter->domain, $result->{'domain'});
+        assertEquals($this->filter->scheme, $result->{'scheme'});
+    }   
+
+    public function testContainedBy(): void
+    {
+        $result = $this->filter->containedBy('name', 'Algeria');
+        $reflection = new ReflectionClass($result->{'url'});
+        $property = $reflection->getProperty('query');
+        $property->setAccessible(true);
+        print_r($property->getValue($result->{'url'}));
+        ob_flush();
+        assertEquals($this->filter->path, $result->{'path'});
+        assertEquals($this->filter->domain, $result->{'domain'});
+        assertEquals($this->filter->scheme, $result->{'scheme'});
+    }   
+
 }
