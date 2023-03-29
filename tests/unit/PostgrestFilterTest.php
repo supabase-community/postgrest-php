effd872d8a756d0bddd84e72473ee3c0e2565ec7
Norm <tehnorm@users.noreply.github.com>
2023-03-29 19:18:17 +0000

    Apply fixes from StyleCI

--- tests/unit/PostgrestFilterTest.php
+++ tests/unit/PostgrestFilterTest.php
@@ -27,132 +27,132 @@ final class PostgrestFilterTest extends TestCase
     public function testEq(): void
     {
         $result = $this->filter->eq('name', 'Algeria');
-        assertEquals('https://gpdefvsxamnscceccczu.supabase.co?name=eq.Algeria',$result->url->__toString());
+        assertEquals('https://gpdefvsxamnscceccczu.supabase.co?name=eq.Algeria', $result->url->__toString());
     }
 
     public function testNeq(): void
     {
         $result = $this->filter->neq('name', 'Algeria');
-        assertEquals('https://gpdefvsxamnscceccczu.supabase.co?name=neq.Algeria',$result->url->__toString());
+        assertEquals('https://gpdefvsxamnscceccczu.supabase.co?name=neq.Algeria', $result->url->__toString());
     }
 
     public function testGt(): void
     {
         $result = $this->filter->gt('id', '1');
-        assertEquals('https://gpdefvsxamnscceccczu.supabase.co?id=gt.1',$result->url->__toString());
+        assertEquals('https://gpdefvsxamnscceccczu.supabase.co?id=gt.1', $result->url->__toString());
     }
 
     public function testGte(): void
     {
         $result = $this->filter->gte('id', '1');
-        assertEquals('https://gpdefvsxamnscceccczu.supabase.co?id=gte.1',$result->url->__toString());
+        assertEquals('https://gpdefvsxamnscceccczu.supabase.co?id=gte.1', $result->url->__toString());
     }
 
     public function testLt(): void
     {
         $result = $this->filter->lt('id', '1');
-        assertEquals('https://gpdefvsxamnscceccczu.supabase.co?id=lt.1',$result->url->__toString());
+        assertEquals('https://gpdefvsxamnscceccczu.supabase.co?id=lt.1', $result->url->__toString());
     }
 
     public function testLte(): void
     {
         $result = $this->filter->lte('id', '1');
-        assertEquals('https://gpdefvsxamnscceccczu.supabase.co?id=lte.1',$result->url->__toString());
+        assertEquals('https://gpdefvsxamnscceccczu.supabase.co?id=lte.1', $result->url->__toString());
     }
 
     public function testLike(): void
     {
         $result = $this->filter->like('name', 'Algeria');
-        assertEquals('https://gpdefvsxamnscceccczu.supabase.co?name=like.Algeria',$result->url->__toString());
+        assertEquals('https://gpdefvsxamnscceccczu.supabase.co?name=like.Algeria', $result->url->__toString());
     }
 
     public function testiLike(): void
     {
         $result = $this->filter->ilike('name', 'Algeria');
-        assertEquals('https://gpdefvsxamnscceccczu.supabase.co?name=ilike.Algeria',$result->url->__toString());
+        assertEquals('https://gpdefvsxamnscceccczu.supabase.co?name=ilike.Algeria', $result->url->__toString());
     }
 
     public function testIs(): void
     {
         $result = $this->filter->is('name', 'Algeria');
-        assertEquals('https://gpdefvsxamnscceccczu.supabase.co?name=is.Algeria',$result->url->__toString());
+        assertEquals('https://gpdefvsxamnscceccczu.supabase.co?name=is.Algeria', $result->url->__toString());
     }
 
     public function testIn(): void
     {
         $result = $this->filter->in('Algeria', ['countries', 'id']);
-        assertEquals('https://gpdefvsxamnscceccczu.supabase.co?Algeria=in.%28countries%2Cid%29',$result->url->__toString());
+        assertEquals('https://gpdefvsxamnscceccczu.supabase.co?Algeria=in.%28countries%2Cid%29', $result->url->__toString());
     }
 
     public function testContains(): void
     {
         $result = $this->filter->contains('name', 'Algeria');
-        assertEquals('https://gpdefvsxamnscceccczu.supabase.co?name=cs.Algeria',$result->url->__toString());
+        assertEquals('https://gpdefvsxamnscceccczu.supabase.co?name=cs.Algeria', $result->url->__toString());
     }
 
     public function testContainedBy(): void
     {
         $result = $this->filter->containedBy('name', 'Algeria');
-        assertEquals('https://gpdefvsxamnscceccczu.supabase.co?name=cd.Algeria',$result->url->__toString());
+        assertEquals('https://gpdefvsxamnscceccczu.supabase.co?name=cd.Algeria', $result->url->__toString());
     }
 
     public function testRangeGt(): void
     {
         $result = $this->filter->rangeGt('name', 'Algeria');
-        assertEquals('https://gpdefvsxamnscceccczu.supabase.co?name=sr.Algeria',$result->url->__toString());
+        assertEquals('https://gpdefvsxamnscceccczu.supabase.co?name=sr.Algeria', $result->url->__toString());
     }
 
     public function testRangeGte(): void
     {
         $result = $this->filter->rangeGte('name', 'Algeria');
-        assertEquals('https://gpdefvsxamnscceccczu.supabase.co?name=nxl.Algeria',$result->url->__toString());
+        assertEquals('https://gpdefvsxamnscceccczu.supabase.co?name=nxl.Algeria', $result->url->__toString());
     }
 
     public function testRangeLt(): void
     {
         $result = $this->filter->rangeLt('name', 'Algeria');
-        assertEquals('https://gpdefvsxamnscceccczu.supabase.co?name=sl.Algeria',$result->url->__toString());
+        assertEquals('https://gpdefvsxamnscceccczu.supabase.co?name=sl.Algeria', $result->url->__toString());
     }
 
     public function testRangeLte(): void
     {
         $result = $this->filter->rangeLte('name', 'Algeria');
-        assertEquals('https://gpdefvsxamnscceccczu.supabase.co?name=nxr.Algeria',$result->url->__toString());
+        assertEquals('https://gpdefvsxamnscceccczu.supabase.co?name=nxr.Algeria', $result->url->__toString());
     }
 
     public function testRangeAdjacent(): void
     {
         $result = $this->filter->rangeAdjacent('name', 'Algeria');
-        assertEquals('https://gpdefvsxamnscceccczu.supabase.co?name=adj.Algeria',$result->url->__toString());
+        assertEquals('https://gpdefvsxamnscceccczu.supabase.co?name=adj.Algeria', $result->url->__toString());
     }
 
     public function testOverlaps(): void
     {
         $result = $this->filter->overlaps('name', 'Algeria');
-        assertEquals('https://gpdefvsxamnscceccczu.supabase.co?name=ov.Algeria',$result->url->__toString());
+        assertEquals('https://gpdefvsxamnscceccczu.supabase.co?name=ov.Algeria', $result->url->__toString());
     }
 
     public function testMatch(): void
     {
         $result = $this->filter->match('name', 'Algeria');
-        assertEquals('https://gpdefvsxamnscceccczu.supabase.co',$result->url->__toString());
+        assertEquals('https://gpdefvsxamnscceccczu.supabase.co', $result->url->__toString());
     }
 
     public function testNot(): void
     {
         $result = $this->filter->not('name', 'IS', 'Algeria');
-        assertEquals('https://gpdefvsxamnscceccczu.supabase.co?name=not.IS.Algeria',$result->url->__toString());
+        assertEquals('https://gpdefvsxamnscceccczu.supabase.co?name=not.IS.Algeria', $result->url->__toString());
     }
 
     public function testOr(): void
     {
         $result = $this->filter->or('name', 'Algeria');
-        assertEquals('https://gpdefvsxamnscceccczu.supabase.co?0=or&1=%28name%29',$result->url->__toString());
+        assertEquals('https://gpdefvsxamnscceccczu.supabase.co?0=or&1=%28name%29', $result->url->__toString());
     }
 
     public function testFilter(): void
     {
         $result = $this->filter->filter('name', 'IS', 'Algeria');
-        assertEquals('https://gpdefvsxamnscceccczu.supabase.co?name=IS.Algeria',$result->url->__toString());
+        assertEquals('https://gpdefvsxamnscceccczu.supabase.co?name=IS.Algeria', $result->url->__toString());
     }
 }
