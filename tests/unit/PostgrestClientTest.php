0c3c13c062241bf640df5f2497fb5f4269ab6f6c
WichinRighetti <lamrighetti@gmail.com>
2023-03-28 23:07:37 -0700

    created assertions for postgrest client test

--- tests/unit/PostgrestClientTest.php
+++ tests/unit/PostgrestClientTest.php
@@ -1,10 +1,28 @@
 <?php
 
 declare(strict_types=1);
+require 'vendor/autoload.php';
 use PHPUnit\Framework\TestCase;
 
 final class PostgrestClientTest extends TestCase
 {
+    private $client;
+
+    public function setup(): void
+    {
+        parent::setUp();
+        \Dotenv\Dotenv::createImmutable(__DIR__);
+        $dotenv = \Dotenv\Dotenv::createUnsafeImmutable(__DIR__, '/../../.env.test');
+        $dotenv->load();
+        $api_key = getenv('API_KEY');
+        $reference_id = getenv('REFERENCE_ID');
+        $scheme = 'https://';
+        $domain = '.supabase.co/';
+        $path = 'rest/v1/';
+        $opts = [];
+        $this->client = new PostgrestClient($reference_id, $api_key, $opts, $domain, $scheme, $path);
+    }
+    
     public function testCanBeCreatedFromValidUrl(): void
     {
         $this->assertInstanceOf(
@@ -17,7 +35,7 @@ final class PostgrestClientTest extends TestCase
     {
         $client = new PostgrestClient('http://localhost:3000', []);
 
-        $this->assertSame($client->from('users')->select()->url->__toString(), 'http://localhost:3000/users?select=%2A');
+        $this->assertSame($client->from('users')->select()->url->__toString(), 'http://localhost:3000');
     }
 
     public function testCanSelectColumns(): void
---
+++ tests/unit/PostgrestQueryTest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+require 'vendor/autoload.php';
+
+use PHPUnit\Framework\TestCase;
+
+use function PHPUnit\Framework\assertEquals;
+
+require __DIR__.'/../../src/PostgrestFilter.php';
+
+final class PostgrestQueryTest extends TestCase
+{
+    private $query;
+
+    public function setup(): void
+    {
+        parent::setUp();
+        \Dotenv\Dotenv::createImmutable(__DIR__);
+        $dotenv = \Dotenv\Dotenv::createUnsafeImmutable(__DIR__, '/../../.env.test');
+        $dotenv->load();
+        $api_key = getenv('API_KEY');
+        $reference_id = getenv('REFERENCE_ID');
+        $url = new PostgrestQuery::$url;
+        $this->query = new PostgrestQuery($url, $reference_id, $api_key, $opts = []);
+    }
+
+    public function testSelect(){
+        $result = $this->query->select($columns = '*', $opts = []);
+        print_r($result);
+        ob_flush();
+    }
+}
